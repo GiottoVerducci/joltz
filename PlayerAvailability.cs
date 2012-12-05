4698ce5f547d3bbd78dd1cd39bc9046e5a70ca24
v.ripoll@gmail.com <v.ripoll@gmail.com@e3586c6d-3f80-e2d5-9b51-f3df55ed1f6d>
2012-12-05 10:56:07 +0000

    Added player. Added "uncertain" mode.

--- PlayerAvailability.cs
+++ PlayerAvailability.cs
@@ -26,16 +26,20 @@ namespace JOLTZ
             GmtOffset = Convert.ToInt32(items[1].Substring(3));
             for (var i = 2; i < items.Length; ++i)
             {
+                bool isUncertain = items[i][0] == 'u';
+                if (isUncertain)
+                    items[i] = items[i].Substring(1);
                 var ranges = items[i].Split('-', ':');
-                AddAvailability(Convert.ToInt32(ranges[0]), Convert.ToInt32(ranges[1]), Convert.ToInt32(ranges[2]), Convert.ToInt32(ranges[3]));
+                AddAvailability(Convert.ToInt32(ranges[0]), Convert.ToInt32(ranges[1]), Convert.ToInt32(ranges[2]), Convert.ToInt32(ranges[3]), isUncertain);
             }
         }
 
-        private void AddAvailability(int startHour, int startMinute, int endHour, int endMinute)
+        private void AddAvailability(int startHour, int startMinute, int endHour, int endMinute, bool isUncertain)
         {
             Availabilities.Add(new Availability(
                 new DateTime(2012, 1, 1, GetUtcHour(startHour), startMinute, 0, 0, DateTimeKind.Utc),
-                new DateTime(2012, 1, 1, GetUtcHour(endHour), endMinute, 0, 0, DateTimeKind.Utc)));
+                new DateTime(2012, 1, 1, GetUtcHour(endHour), endMinute, 0, 0, DateTimeKind.Utc),
+                isUncertain));
         }
 
         public int GetUtcHour(int hour)
